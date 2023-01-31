0cdfafce1351218d18c23d16071dfc076604a9ed
TerroFlys <35329399+TerroFlys@users.noreply.github.com>
2023-01-31 13:35:11 +0100

    Moved logic, if player does not have permission, does not receive notification

--- src/main/java/com/terroflys/lastdeath/listeners/DeathListener.java
+++ src/main/java/com/terroflys/lastdeath/listeners/DeathListener.java
@@ -18,29 +18,31 @@ public class DeathListener implements Listener {
                 return "Overworld";
         }
     }
+
     @EventHandler
     public void onDeath(PlayerDeathEvent event) {
-        //This line shows colors
-        String lastdeath = ChatColor.GREEN + "You have died at " + ChatColor.RED + "[X: " +
-                event.getEntity().getLocation().getBlockX() + ", Y: " + event.getEntity().getLocation().getBlockY() + ", Z: "+ event.getEntity().getLocation().getBlockZ() + "] " + ChatColor.GREEN +" in the "
-                + ChatColor.RED+ getWorldDisplayName(event.getEntity().getWorld().getEnvironment().name()) + ChatColor.GREEN + ".";
-        //SEND TO PLAYER
-        //This does not show colors
-        event.getEntity().sendMessage(lastdeath);
-
-
-        //SAVE INSIDE PLUGIN's UTILS
-        saveLastDeath(event.getEntity().getUniqueId(), lastdeath);
-
-        //LOG TO CONSOLE
+        //LOG TO CONSOLE EVEN IF THEY DO NOT HAVE PERMISSION
         event.getEntity().getServer().getLogger().info(event.getEntity().getName() + " has died at " + "[X: " +
                 event.getEntity().getLocation().getBlockX() + ", Y: " + event.getEntity().getLocation().getBlockY() + ", Z: " +
                 event.getEntity().getLocation().getBlockZ() + "] " + " in the "
                 + getWorldDisplayName(event.getEntity().getWorld().getEnvironment().name()) + ".");
 
 
-        //SAVE TO FILE ??
+        //This line shows colors
+        String lastdeath = ChatColor.GREEN + "You have died at " + ChatColor.RED + "[X: " +
+                event.getEntity().getLocation().getBlockX() + ", Y: " + event.getEntity().getLocation().getBlockY() + ", Z: " + event.getEntity().getLocation().getBlockZ() + "] " + ChatColor.GREEN + " in the "
+                + ChatColor.RED + getWorldDisplayName(event.getEntity().getWorld().getEnvironment().name()) + ChatColor.GREEN + ".";
+        //SAVE INSIDE PLUGIN's UTILS
 
+        //RECORD DEATH EVEN WITHOUT ACTIVE PERMISSION
+        saveLastDeath(event.getEntity().getUniqueId(), lastdeath);
+
+        //if player does not have permission, do not send message
+        if (!event.getEntity().getPlayer().hasPermission("lastdeath.notify")) return;
+
+
+        //SEND TO PLAYER
+        event.getEntity().sendMessage(lastdeath);
 
     }
 }
